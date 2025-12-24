be364d49d5b0e10069aeb0b6aca2544b21ab156d
d-schultz-15 <126746803+d-schultz-15@users.noreply.github.com>
2025-12-23 20:47:46 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,24 @@
 window.LOCATIONS = [
   {
     id: "cz400-heli-sunset",
-    name: "Daytona International Speedway",
+    name: "Touchdown in Daytona",
     coords: [29.1919290, -81.0678660],
-    trip: "florida", // <-- add this
-    photos: [{ src: "./photos/coke-zero-400-2025.jpg", caption: "Helicopters Flying Over Speedway." }]
+    trip: "No Trip",
+    photos: [{ src: "./photos/coke-zero-400-2025.jpg", caption: "This photo was taken at the beginning of the 2025 Coke Zero 400 race." }]
   },
   {
     id: "new-york-city",
-    name: "New York City",
-    coords: [40.7128, -74.0060],
-    trip: "east_coast", // <-- red
-    photos: [{ src: "./photos/essex-house.jpg", caption: "Essex House seen through fog." }]
+    name: "Essex House",
+    coords: [40.703019, -74.014416],
+    trip: "east_coast",
+    photos: [{ src: "./photos/essex-house.jpg", caption: "A foggy photo of the Essex House from New York City's financial district." }]
   },
   {
     id: "new-york-city-2",
-    name: "New York City",
-    coords: [40.7128, -74.007],
-    trip: "other_trip", // <-- different color
-    photos: [{ src: "./photos/yellow-taxi-nyc.jpg", caption: "Yellow taxi in NYC." }]
+    name: "Yellow Highlight",
+    coords: [40.703586, -74.013409],
+    trip: "east_coast",
+    photos: [{ src: "./photos/yellow-taxi-nyc.jpg", caption: "A yellow taxi highlighted on the dark streets of New York City." }]
   },
 ];
+
